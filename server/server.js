3c4754a8f8c4f2532801e1b04fe19178501a8770
catlam <catlam06062003@gmail.com>
2025-03-29 01:13:57 +0700

    Initial commit

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ import categoriesRouter from "./Routes/CategoriesRouter.js";
 import Uploadrouter from './Controllers/UploadFile.js';
 
 dotenv.config();
+import path from "path";
+
 
 const app = express();
 app.use(cors());
@@ -33,6 +35,15 @@ app.use("/api/upload", Uploadrouter);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', "build", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
